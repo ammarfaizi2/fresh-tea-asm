e7fc385c0365267c6d27958014c7e89f7053c10c
Ammar Faizi <ammarfaizi2@gmail.com>
2019-09-07 21:24:32 +0700

    Update d

--- examples/x64/advanced_change_phpval.php
+++ examples/x64/advanced_change_phpval.php
@@ -1,16 +1,18 @@
 <?php
 require __DIR__."/../../src/autoload.php";
+
 $code = <<<CODE
 	mov rax, [rdi]
   	mov rax, [rax]
-  	add rax, 16
-  	sub qword [rax], 7
-  	ret
+
+  	;; Write your code here...
+
 CODE;
+
 $x64 = new PhpNasm\Arch\x64($code);
 $code = $x64->compile();
-$a = "PHP Indonesia for Student abcdef";
+$a = "You doesn't have any objection to it, right?";
 $exe = new PhpNasm\Executor($code);
 unset($code);
 $exe->execute($a);
-var_dump($a); // PHP Indonesia for Student
+var_dump($a); // You don't have any objection to it, right?

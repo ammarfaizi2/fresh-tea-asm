2c6f560932d9ad2f8cd8d1a2530ef3579e044ded
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-04 22:05:03 +0700

    Add memcpy

--- examples/x64/memcpy.php
+++ examples/x64/memcpy.php
@@ -1,10 +1,19 @@
 <?php
 require __DIR__."/../../src/autoload.php";
 $code = <<<CODE
-
-	; Write your code here...
-
-	ret
+    mov r9, [rdi]
+    mov r9, [r9]
+    mov rax, [rdi + 8]
+    mov rax, [rax]
+    mov rdi, [rdi + 16]
+    mov rdi, [rdi]
+.L1:
+    dec r9
+    mov sil, byte [rax + r9 + 24]
+    mov byte [rdi + r9 + 24], sil
+    cmp r9, 0
+    jge .L1
+    ret
 CODE;
 
 $x64 = new PhpNasm\Arch\x64($code);
@@ -12,7 +21,7 @@ $code = $x64->compile();
 
 $a = "....................";
 $b = "Hello World!";
-$c = 8;
+$c = 12;
 
 $exe = new PhpNasm\Executor($code);
 $exe->execute($a, $b, $c);

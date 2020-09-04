1db95b5ec957c4a4e32ede9b600654b998cba437
Ammar Faizi <ammarfaizi2@gmail.com>
2020-09-04 16:09:55 +0700

    Update build.php

--- build.php
+++ build.php
@@ -2,10 +2,10 @@
 
 require __DIR__."/light-tea/framework/autoload.php";
 
-LightTeaPHP::setExtName("helloworld");
-LightTeaPHP::setExtDescription("Hello World Program");
-LightTeaPHP::setBuildDir(__DIR__."/build");
-LightTeaPHP::setSourceDir(__DIR__."/src");
+LightTeaPHP::setExtName("fresh_tea_asm");
+LightTeaPHP::setExtDescription("Fresh Tea ASM");
+LightTeaPHP::setBuildDir(__DIR__."/src");
+LightTeaPHP::setSourceDir(__DIR__."/src.frag");
 
 LightTeaPHP::setPrePattern("/^(.*)\.php\.(s|c|h|cc|cpp|hpp|asm)$/S");
 LightTeaPHP::setCopyPattern("/^.*\.(s|c|h|cc|cpp|hpp|asm)$/S");
